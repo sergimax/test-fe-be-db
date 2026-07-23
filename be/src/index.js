447c8ab3a145185c47ad65c0f7ddbd0de4b32d29
Maksim Sergienko <creativesun@yandex.ru>
2026-07-23 16:58:20 +0300

    feat(be, fe): update backend response to include message and timestamp; add fetchData function in frontend to retrieve and log backend message

--- be/src/index.js
+++ be/src/index.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const message = {
+    message: 'Hello from the backend!',
+    timestamp: new Date().toISOString()
+  }
+  res.send(message)
 })
 
 app.listen(port, () => {
--- fe/src/index.js
+++ fe/src/index.js
@@ -1,5 +1,13 @@
 function main() {
     console.log('Hello World');
+    fetchData();
+}
+
+function fetchData() {
+    fetch('http://localhost:3000')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
 }
 
 main();
